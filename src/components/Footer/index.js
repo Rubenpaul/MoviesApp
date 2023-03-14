f3a2017a25dafaa7f50ad28628bc44fdb56236b8
Rubenpaul <rubenpaul29@gmail.com>
2023-03-14 16:16:45 +0000

    added code

---
+++ src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+import {FaGoogle, FaTwitter, FaInstagram, FaYoutube} from 'react-icons/fa'
+
+const Footer = () => (
+  <div className="footer">
+    <ul className="footer-items-list">
+      <li className="footer-item">
+        <button type="button" className="button">
+          <FaGoogle size={16} />
+        </button>
+      </li>
+      <li className="footer-item">
+        <button type="button" className="button">
+          <FaTwitter size={16} />
+        </button>
+      </li>
+      <li className="footer-item">
+        <button type="button" className="button">
+          <FaInstagram size={16} />
+        </button>
+      </li>
+      <li className="footer-item">
+        <button type="button" className="button">
+          <FaYoutube size={16} />
+        </button>
+      </li>
+    </ul>
+    <p className="contact-us">Contact Us</p>
+  </div>
+)
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,150 @@
+import './index.css'
+import {Component} from 'react'
+
+import {Link} from 'react-router-dom'
+
+import {HiOutlineSearch} from 'react-icons/hi'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiFillCloseCircle} from 'react-icons/ai'
+
+class Header extends Component {
+  state = {showSearchInput: false, showMenu: false, searchInput: ''}
+
+  onClickSearchBtn = () => {
+    const {searchInput, showSearchInput} = this.state
+
+    if (searchInput !== '' && showSearchInput === true) {
+      console.log('Go to search Page')
+    } else {
+      this.setState(prevState => ({
+        showSearchInput: !prevState.showSearchInput,
+      }))
+    }
+  }
+
+  onClickHamburgerMenu = () => {
+    this.setState(prevState => ({
+      showMenu: !prevState.showMenu,
+    }))
+  }
+
+  onClickCloseBtn = () => {
+    this.setState({showMenu: false})
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {showSearchInput, showMenu, searchInput} = this.state
+
+    const searchContainerStyle = showSearchInput
+      ? 'search-container'
+      : 'new-search-container'
+
+    const searchBtnContainerStyle = showSearchInput
+      ? 'search-btn-container'
+      : 'new-search-btn-container'
+
+    return (
+      <div className="header">
+        <nav className="nav">
+          <div className="nav-container">
+            <Link to="/">
+              <img
+                src="https://res.cloudinary.com/dio2gwquj/image/upload/v1678711969/Group_7399_movies_logo_hvsrde.png"
+                alt="website logo"
+                className="website-logo"
+              />
+            </Link>
+
+            <div className="nav-items-container">
+              <ul className="nav-list">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/popular" className="nav-link">
+                    Popular
+                  </Link>
+                </li>
+              </ul>
+
+              <div className="nav-details-container">
+                <div className={searchContainerStyle}>
+                  {showSearchInput && (
+                    <input
+                      type="search"
+                      className="search-input"
+                      value={searchInput}
+                      onChange={this.onChangeSearchInput}
+                    />
+                  )}
+                  <div className={searchBtnContainerStyle}>
+                    <button
+                      type="button"
+                      className="search-btn"
+                      onClick={this.onClickSearchBtn}
+                    >
+                      <HiOutlineSearch size={24} />
+                    </button>
+                  </div>
+                </div>
+                <div className="profile-btn-container">
+                  <Link to="/account" className="profile-link">
+                    <button type="button" className="profile-btn">
+                      <img
+                        src="https://res.cloudinary.com/dio2gwquj/image/upload/v1678799136/Mask_Group_account-icon_bdjfsb.png"
+                        alt="profile"
+                      />
+                    </button>
+                  </Link>
+                </div>
+
+                <button
+                  type="button"
+                  className="hamburger-btn"
+                  onClick={this.onClickHamburgerMenu}
+                >
+                  <GiHamburgerMenu size={24} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+        {showMenu && (
+          <div className="menu">
+            <ul className="menu-items">
+              <li className="menu-item">
+                <Link to="/" className="menu-link">
+                  Home
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/popular" className="menu-link">
+                  Popular
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/account" className="menu-link">
+                  Account
+                </Link>
+              </li>
+            </ul>
+            <button
+              type="button"
+              className="close-btn"
+              onClick={this.onClickCloseBtn}
+            >
+              <AiFillCloseCircle size={18} />
+            </button>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+export default Header
